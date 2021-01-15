c5516e8ba9cf5e4b9aa535977ed4b975b28dfeda
Lenocka <lvutina.en@gmail.com>
2021-01-15 14:39:08 +0300

    feat:add product card with styles

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -5,18 +5,18 @@ function Card(props) {
     const {imgSrc, figCaption, price} = props;
     return (
         <div className="Card">
-
             <figure className='Card-image'>
                 <img src={imgSrc}
-                     alt={figCaption}/>
-                    <figcaption>{ figCaption }</figcaption>
+                     alt={figCaption}
+                    className='Card-picture'/>
+                    <figcaption className='Card-image_figcaption'>{ figCaption }</figcaption>
             </figure>
-               <span className='Card-price'>
-                   {price}
-               </span>
-                <button className='Card-button__buy'>
+            <span className='Card-price'>
+                   {Number(price).toLocaleString('ru')}
+            </span>
+            <button className='Card-button__buy'>
                     Купить
-                </button>
+            </button>
         </div>
     );
 }
